df4682dc927d9fdb262a2c3538d5a7c0e1e455f3
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2019-12-03 12:59:08 -0800

    first commit, created react app and changed functional app to class

---
+++ github-api-users/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+  state = {
+    users: [],
+  }
+
+  componentsDidMount() {
+
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
